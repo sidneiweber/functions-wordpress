c68d65c9102d1d7bb4b51003945e3e75531b5238
sidneiweber <sidnei.weber@gmail.com>
2014-05-21 11:27:10 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ add_filter('admin_footer_text', 'wpmidia_change_footer_admin');
 
 if (! current_user_can ('edit_users')) {
   add_action ('init', create_function ('$ a "," remove_action (' init ',' wp_version_check '); "), 2);
-  add_filter ('pre_option_update_core', create_function ('$ a "," return null; "));
+  add_filter ('pre_option_update_core', create_function ('$' a "," return null; "));
 }
 
 // Alterando a cor da Admin Bar
